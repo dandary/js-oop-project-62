6f4d5c86a247d331c23c147f5284718403f6e134
Dandary <mukuro489@gmail.com>
2023-12-15 12:39:27 -0800

    step1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/dandary/js-oop-project-62/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/dandary/js-oop-project-62/actions)
+cdcc2730218c39705a2b3fdc5bfca0223eeec5667c03f3cb3d058845c69569a8
---
+++ src/Validator.js
@@ -0,0 +1,41 @@
+/* eslint-disable class-methods-use-this */
+import ArraySchema from './ArraySchema.js';
+import NumberSchema from './NumberSchema.js';
+import ObjectSchema from './ObjectSchema.js';
+import StringSchema from './StringSchema.js';
+
+class Validator {
+  constructor() {
+    this.moreValidationFuncs = {
+      string: {},
+      number: {},
+      array: {},
+      object: {},
+    };
+  }
+
+  addValidator(type, name, fn) {
+    if (type in this.moreValidationFuncs && typeof fn === 'function') {
+      this.moreValidationFuncs[type][name] = fn;
+    }
+    return this;
+  }
+
+  string() {
+    return new StringSchema(this.moreValidationFuncs.string);
+  }
+
+  number() {
+    return new NumberSchema(this.moreValidationFuncs.number);
+  }
+
+  array() {
+    return new ArraySchema(this.moreValidationFuncs.array);
+  }
+
+  object() {
+    return new ObjectSchema(this.moreValidationFuncs.object);
+  }
+}
+
+export default Validator;
