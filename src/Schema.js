50d35b3a0670b4c758024bde14a05e1a160819fa
Dandary <mukuro489@gmail.com>
2023-12-27 12:17:48 -0800

    modified:   __tests__/ArraySchema.test.js #	modified:   __tests__/NumberSchema.test.js #	modified:   __tests__/ObjectSchema.test.js #	modified:   __tests__/StringSchema.test.js #	modified:   __tests__/Validator.test.js #	modified:   src/Schema.js

--- src/Schema.js
+++ src/Schema.js
@@ -1,51 +1,49 @@
 class Schema {
-    constructor(moreValidationFuncs = {}) {
-      this.predicates = [];
-      this.obligatory = false;
-      this.moreValidationFuncs = { ...moreValidationFuncs };
-    }
-  
-    isValid(value) {
-      const totalPredicate = this.buildTotalPredicate();
-      return totalPredicate(value);
-    }
-  
-    required() {
-      this.obligatory = true;
-      return this;
-    }
-  
-    test(funcName, ...args) {
-      if (funcName in this.moreValidationFuncs) {
-        const fn = this.moreValidationFuncs[funcName].bind(this);
-        this.predicates.push(
-          (v) => v === undefined || v === null || fn(v, ...args),
-        );
-      }
-      return this;
-    }
-  
-    buildTotalPredicate() {
-      return (value) => this.getAllPredicates().every((predFn) => predFn(value));
-    }
-  
-    getAllPredicates() {
-      const prerequisites = [this.constructor.getTypePredicate()];
-      if (this.obligatory) {
-        prerequisites.push(this.constructor.getRequiredPredicate());
-      }
-      return prerequisites.concat(this.predicates);
-    }
-  
-    static getTypePredicate() {
-      return () => true;
-    }
-  
-    static getRequiredPredicate() {
-      return (v) => v !== undefined && v !== null;
-    }
+  constructor(moreValidationFuncs = {}) {
+    this.predicates = [];
+    this.obligatory = false;
+    this.moreValidationFuncs = { ...moreValidationFuncs };
   }
-  
-  export default Schema;
-  
-  
+
+  isValid(value) {
+    const totalPredicate = this.buildTotalPredicate();
+    return totalPredicate(value);
+  }
+
+  required() {
+    this.obligatory = true;
+    return this;
+  }
+
+  test(funcName, ...args) {
+    if (funcName in this.moreValidationFuncs) {
+      const fn = this.moreValidationFuncs[funcName].bind(this);
+      this.predicates.push(
+        (v) => v === undefined || v === null || fn(v, ...args),
+      );
+    }
+    return this;
+  }
+
+  buildTotalPredicate() {
+    return (value) => this.getAllPredicates().every((predFn) => predFn(value));
+  }
+
+  getAllPredicates() {
+    const prerequisites = [this.constructor.getTypePredicate()];
+    if (this.obligatory) {
+      prerequisites.push(this.constructor.getRequiredPredicate());
+    }
+    return prerequisites.concat(this.predicates);
+  }
+
+  static getTypePredicate() {
+    return () => true;
+  }
+
+  static getRequiredPredicate() {
+    return (v) => v !== undefined && v !== null;
+  }
+}
+
+export default Schema;
